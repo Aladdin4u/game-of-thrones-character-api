673428286f6fcc77ef9917fbbaa67a36cde37019
Aladdin4u <taofikakinkumi@gmail.com>
2022-06-14 16:54:44 +0100

    add image and readme file

---
+++ README.md
@@ -0,0 +1,25 @@
+# Game of Throne API
+A an api that shows Game of Thrones house character.
+
+**Link to project:** https://game-of-throne-api.herokuapp.com/
+
+![alt tag](img/gameofthroneshome.jpg)
+![alt tag](img/fetchdata.jpg)
+
+## How It's Made:
+
+**Tech used:** HTML, CSS, JavaScript, Node.js Express
+
+This api runs on Node.js with the help of express. When you send a request to the /houses page it return a json object with character list. Each house has a name and a code.
+
+The html, css and javascript files serve as a landing page with documentation on how to use the api 
+
+## Optimizations
+
+I consistenly refactor and optimize my code, I would like to add image of each house characters for better UI for the users.
+
+## Lessons Learned:
+
+I learned how to use node to serve json data with express framework. I also learned to deploy a project to heroku via the command line.
+
+
--- main.js
+++ main.js
@@ -1,15 +1,25 @@
 document.getElementById("clickMe").addEventListener("click", apiRequest);
 
-async function apiRequest () {
-   const rappersName = document.querySelector('rapper').value; 
+async function apiRequest () { 
+    document.getElementById('characters').innerHTML="";
    try {
-    const response = await fetch(`https://game-of-throne-api.herokuapp.com/api/houses/${rappersName}`)
+    const house = document.querySelector('input').value; 
+    const response = await fetch(`https://game-of-throne-api.herokuapp.com/api/houses/${house}`)
     const data = await response.json()
 
     console.log(data)
-    document.querySelector('h2').innerHTML = data.name
+    const characterElement = data.character;
+    document.getElementById('charHouse').textContent = `House of ${house} character are:`
+    
+    characterElement.forEach(element => {
+        const character = document.querySelector('#characters');
+        const listElement = document.createElement('li');
+        listElement.textContent= element;
+        character.appendChild(listElement);
+    })
    }
     catch(error) {
         console.log(error)
+        document.getElementById('characters').innerHTML= "Wrong house name";
     }
 }
